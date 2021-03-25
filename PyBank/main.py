c2939389490af2b3b038c8d6228cf70991afdbee
William Serene <willserene@gmail.com>
2021-03-25 13:34:06 -0600

    monthlychangeprogress

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,8 +3,17 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-total_months = 0
+total_months = 1
 net_total = 0
+monthly_change_list = []
+
+
+# average_change = mean monthly change
+# greatest_increase = max monthly change
+# greatest_decrease = min monthly change
+
+# create variable that equals month 
+
 
 # Open and read csv
 with open(csvpath) as budget_data:
@@ -12,15 +21,31 @@ with open(csvpath) as budget_data:
     
 
 # The total number of months in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+    
     header = next(reader) # skips header
+    previous_month = next(reader)
+    net_total = previous_month
     for row in reader:
         total_months += 1 # adds 1 for each row
         net_total += int(row[1])
-    print(f'Total Months: {total_months}')
-    print(f'Net Total Profit: ${net_total}')
+        
+        monthly_change = (int(row[1])-int(previous_month[1]))
+        previous_month = int(row[1])
+
+        monthly_change_list.append(monthly_change)
 
 
-# The net total amount of "Profit/Losses" over the entire period
+        
+
+
+
+
+    # print(f'Total Months: {total_months}') Testing
+    # print(f'Net Total Profit: ${net_total}') Testing
+
+        
+
 
         
 
