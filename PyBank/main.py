edb371a21fe2868699e7f6b8b7b7ca7ed6332762
William Serene <willserene@gmail.com>
2021-03-27 16:41:34 -0600

    ithinkimdone

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,88 +7,50 @@ total_months = 1
 net_total = 0
 monthly_change = 0
 monthly_change_list = []
-months = []
 
-# average_change = mean monthly change
-# greatest_increase = max monthly change
-# greatest_decrease = min monthly change
-
-# create variable that equals month 
-
+max_monthly_increase = ["", 0]
+max_monthly_decrease = ["", 0]
 
 # Open and read csv
 with open(csvpath) as budget_data:
     reader = csv.reader(budget_data)
-    
-
-# The total number of months in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-    
+        
     header = next(reader) # skips header
     previous_month = next(reader)  
     net_total = int(previous_month[1])
+    
     for row in reader:
         total_months += 1 # adds 1 for each row
-        
         net_total += int(row[1])
-       
         monthly_change = int(row[1]) - int(previous_month[1]) 
-        
         previous_month = row
-        
         monthly_change_list.append(monthly_change)
 
+        if monthly_change > max_monthly_increase[1]:
+            max_monthly_increase[0] = row[0]
+            max_monthly_increase[1] = monthly_change
+
+        if monthly_change < max_monthly_decrease[1]:
+            max_monthly_decrease[0] = row[0]
+            max_monthly_decrease[1] = monthly_change
+
+
     monthly_average = sum(monthly_change_list) / len(monthly_change_list)
-    
-    max_monthly_increase = max(monthly_change_list)
-    max_monthly_decrease = min(monthly_change_list)
 
-    # matching appropriate month with max increase and max decrease
-    max_increase_month = monthly_change_list.index(max(monthly_change_list)) + 1
-    max_decrease_month = monthly_change_list.index(min(monthly_change_list)) + 1
+output = (
+    f"Financial Analysis\n"
+    f"--------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Profit: ${net_total}\n"
+    f"Average Change: ${monthly_average:.2f}\n"
+    f"Greatest Increase in Profits: {max_monthly_increase[0]} (${max_monthly_increase[1]})\n"
+    f"Greatest Decrease in Profits: {max_monthly_decrease[0]} (${max_monthly_decrease[1]})\n")
+print(output)    
 
-   #  max_month = max_increase_month.append(row[0])
-
-
-
-    # print(f'monthly changes: {monthly_change_list}') # Testing
-    # print(f'previous month: {previous_month}') # Testing
-    # print(f'max increase: {max_month}') Testing
-    # print(f'max increase: {max_month}')
-
-
-    print(f'max monthly increase: ${max_monthly_increase}')
-    print(f'max monthly decrease: ${max_monthly_decrease}')
-    print(f'monthly average: ${monthly_average:.2f}')
-    print(f'Total Months: {total_months}') 
-    print(f'Net Total Profit: ${net_total}')
-    
-
-#   output = (
-#         f"Financial Analysis\n"
-#         f"------------------------\n"
-#         f"Total Months: {total_months}\n"
-#         f"Total Profit: ${net_total}\n"
-#         f"Average Change: ${monthly_average: .2f}\n"
-#         f"Greatest Increase in Profits: \n"
-#         f"Greatest Decrease in Profits: \n")
-#     print(output)
-
-
-    #specify file to write to
-output_path = os.path.join("Analysis", "Financial_Analysis.txt")   
-with open(output_path, 'w', newline = '') as txt_file:
-    csvwriter = csv.writer(txt_file)
-    csvwriter.writerow(['Financial Analysis'])
-        # f"Financial Analysis\n"
-        # f"------------------------\n"
-        # f"Total Months: {total_months}\n"
-        # f"Total Profit: ${net_total}\n"
-        # f"Average Change: ${monthly_average: .2f}\n"
-        # f"Greatest Increase in Profits: \n"
-        # f"Greatest Decrease in Profits: \n")
-    
-    # print(output)
+output_path = open('Analysis/Financial_Analysis.txt', 'w')
+output_path.write(output)
+output_path.close()
+  
 
         
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -26,45 +26,39 @@ with open(csvpath) as csvfile:
         total_votes += 1
         
         if row[2] not in candidates:
-            
             candidates.append(row[2])
-            
             index = candidates.index(row[2])
-           
             candidate_votes.append(1) 
         else:
-           
-            index = candidates.index(row[2])
-
-            candidate_votes[index] += 1
+           index = candidates.index(row[2])
+           candidate_votes[index] += 1
         
-    # print(total_votes)  
-    # 3521001 
-    # print(candidates) 
-    # ['Khan', 'Correy', 'Li', "O'Tooley"]
-    # print(candidate_votes) 
-    # [2218231, 704200, 492940, 105630]
-    
-  
+                        # print(total_votes)  
+                        # 3521001 
+                        # print(candidates) 
+                        # ['Khan', 'Correy', 'Li', "O'Tooley"]
+                        # print(candidate_votes) 
+                        # [2218231, 704200, 492940, 105630]
+      
     Khan_votes = candidate_votes[0]
     Correy_votes = candidate_votes[1]
     Li_votes = candidate_votes[2]
     OTooley_votes = candidate_votes[3]
 
-    # print(Khan_votes)
-    # print(Correy_votes)
-    # print(Li_votes)
-    # print(OTooley_votes)
+                        # print(Khan_votes)
+                        # print(Correy_votes)
+                        # print(Li_votes)
+                        # print(OTooley_votes)
     
     Khan_percent = (Khan_votes / total_votes) * 100
     Correy_percent = (Correy_votes / total_votes) * 100
     Li_percent = (Li_votes / total_votes) * 100
     OTooley_percent = (OTooley_votes / total_votes) * 100
        
-    # print(Khan_percent)
-    # print(Correy_percent)
-    # print(Li_percent)
-    # print(OTooley_percent)
+                        # print(Khan_percent)
+                        # print(Correy_percent)
+                        # print(Li_percent)
+                        # print(OTooley_percent)
 
 output = (
     f"Election Results\n"
@@ -77,9 +71,7 @@ output = (
     f"O'Tooley: {OTooley_percent:.3f}% ({OTooley_votes})\n"
     f"--------------------------\n"
     f"Winner: Khan \n"
-    f"--------------------------\n"
-    
-    )
+    f"--------------------------\n")
 print(output)    
 
 output_path = open('Analysis/Election_Results.txt', 'w')
